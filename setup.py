c80e915af5a98c414f1110f2268875932f44d002
Jason Moore <jason.moore@dbca.wa.gov.au>
2024-08-04 21:38:26 +0800

    Package Install Fixes

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='django5_six',
+      version='1.01',
+      description='Django5 Utils Six',
+      url='https://github.com/xzzy/django5-utils-six',
+      author='Department of Biodiversity, Conservation and Attractions',
+      author_email='asi@dbca.wa.gov.au',
+      license='BSD',
+      packages=[],
+      install_requires=[],
+      include_package_data=True,
+      zip_safe=False)
