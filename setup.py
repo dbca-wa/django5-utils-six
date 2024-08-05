d7de0712672d5ef9a75506e06229dca5b86179e7
Jason Moore <jmoore@austwa.com>
2024-08-05 14:01:28 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='django5_six',
       version='1.01',
       description='Django5 Utils Six',
-      url='https://github.com/xzzy/django5-utils-six',
+      url='https://github.com/dbca-wa/django5-utils-six',
       author='Department of Biodiversity, Conservation and Attractions',
       author_email='asi@dbca.wa.gov.au',
       license='BSD',
